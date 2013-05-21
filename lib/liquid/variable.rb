cdfd1cbb38ae566b8d6ada2e1bcdbbfe67009912
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2013-05-21 14:08:40 -0400

    Fix some old templates that abused colon as an argument separator.
    This is a fallback for keyword argument parsing since this feature broke
    old templates that accidentally used a colon as a filter argument
    separator.

--- lib/liquid/variable.rb
+++ lib/liquid/variable.rb
@@ -23,9 +23,9 @@ module Liquid
         if match[2].match(/#{FilterSeparator}\s*(.*)/o)
           filters = Regexp.last_match(1).scan(FilterParser)
           filters.each do |f|
-            if matches = f.match(/\s*(#{WordRegex}+)(?:\s*#{FilterArgumentSeparator}(.*))?/)
+            if matches = f.match(/\s*(#{WordRegex}+)(.*)/)
               filtername = matches[1]
-              filterargs = matches[2].to_s.scan(/(?:\A|#{ArgumentSeparator})\s*((?:\w+\s*\:\s*)?#{QuotedFragment})/o).flatten
+              filterargs = matches[2].to_s.scan(/(?:#{FilterArgumentSeparator}|#{ArgumentSeparator})\s*((?:\w+\s*\:\s*)?#{QuotedFragment})/o).flatten
               @filters << [filtername, filterargs]
             end
           end
